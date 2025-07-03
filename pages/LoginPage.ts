8690cdf440069c05699c22f21eacf059d371d41f
MarcinNieckosinski <marcin.nieckosinski@gmail.com>
2025-07-03 22:03:28 +0200

    data file added and more test cases for login page

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,12 +5,32 @@ export class LoginPage {
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
+    readonly usernameInputError: Locator;
+    readonly passwordInputError: Locator;
+    readonly usernameTooltip: Locator;
+    readonly passwordTooltip: Locator;
+    readonly usernameTooltipSpan: Locator;
+    readonly passwordTooltipSpan: Locator;
+    readonly securityLink: Locator;
+    readonly usernameKeyboard: Locator;
+    readonly passwordKeyboard: Locator;
+    readonly uiKeyboard: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.usernameInput = page.locator('input#login_id');
         this.passwordInput = page.locator('input#login_password');
         this.loginButton = page.locator('button#login-btn');
+        this.usernameInputError = page.locator('div#error_login_id');
+        this.passwordInputError = page.locator('div#error_login_password');
+        this.usernameTooltip = page.locator('#login_id_container > div.login-tooltip-wrapper > div > i');
+        this.passwordTooltip = page.locator('#login_password_container > div.login-tooltip-wrapper > div > i');
+        this.usernameTooltipSpan = page.locator('#login_id_container > div.login-tooltip-wrapper > div > i > span');
+        this.passwordTooltipSpan = page.locator('#login_password_container > div.login-tooltip-wrapper > div > i > span');
+        this.securityLink = page.locator('a[data-testid="security-link"]');
+        this.usernameKeyboard = page.locator('#kb_login_id');
+        this.passwordKeyboard = page.locator('#login_password_container > div.login-kb.ms-hide > i');
+        this.uiKeyboard = page.locator('div.ui-keyboard');
     }
 
     async goto() {
@@ -21,5 +41,11 @@ export class LoginPage {
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
         await this.loginButton.click();
+    }
+
+    async loginWithEmptyUsernameAndPassword() {
+        await this.usernameInput.click();
+        await this.passwordInput.click();
+        await this.usernameInput.click();
     }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,10 +1,72 @@
 import {test, expect} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
 import {generateRandomWord} from '../utils/DataGenerator';
+import loginPageData from '../data/login_page.json';
 
 test('User can log in with valid credentials', async ({page}) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await loginPage.login(generateRandomWord(8), generateRandomWord(8));
-    await expect(page).toHaveURL('https://demo-bank.vercel.app/pulpit.html');
+    await expect(page).toHaveURL(loginPageData.dashboardUrl);
+});
+
+test('There is an error message when logging in with empty username and password', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.loginWithEmptyUsernameAndPassword();
+    await expect(loginPage.usernameInputError).toHaveText(loginPageData.emptyInputError);
+    await expect(loginPage.passwordInputError).toHaveText(loginPageData.emptyInputError);
+});
+
+test('There is an error message when logging in with too short username and password', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.usernameInput.fill(generateRandomWord(4));
+    await loginPage.passwordInput.fill(generateRandomWord(4));
+    await loginPage.usernameInput.click();
+    expect(await loginPage.usernameInputError.textContent()).toContain(loginPageData.tooShortUsernameError);
+    expect(await loginPage.passwordInputError.textContent()).toContain(loginPageData.tooShortPasswordError);
+});
+
+test('There is a tooltip with information about username requirements', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.usernameTooltip.hover();
+    const tooltipText = await loginPage.usernameTooltipSpan.textContent();
+    expect(tooltipText).toContain(loginPageData.usernameTooltipTextPart1);
+    expect(tooltipText).toContain(loginPageData.usernameTooltipTextPart2);
+});
+
+test('There is a tooltip with information about password requirements', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.passwordTooltip.hover();
+    const tooltipText = await loginPage.passwordTooltipSpan.textContent();
+    expect(tooltipText).toContain(loginPageData.passwordTooltipTextPart1);
+    expect(tooltipText).toContain(loginPageData.passwordTooltipTextPart2);
+    expect(tooltipText).toContain(loginPageData.passwordTooltipTextPart3);
+});
+
+test('There is working keyboard icon for username input', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await expect(loginPage.usernameKeyboard).toBeVisible();
+    await loginPage.usernameKeyboard.click();
+    await expect(loginPage.uiKeyboard).toBeVisible();
+});
+
+test('There is working keyboard icon for password input', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await expect(loginPage.passwordKeyboard).toBeVisible();
+    await loginPage.passwordKeyboard.click();
+    await expect(loginPage.uiKeyboard).toBeVisible();
+});
+
+test('User can click on the security link', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await expect(loginPage.securityLink).toBeVisible();
+    await loginPage.securityLink.click();
+    await expect(page).toHaveURL(loginPageData.securityUrl);
 });
